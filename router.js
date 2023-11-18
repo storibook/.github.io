8250fdb88a17899af5c4865681f9f53bb4f7da0b
amohebbi <amohebbi@mathworks.com>
2023-11-18 17:29:41 -0600

    nlanked out change

--- router.js
+++ router.js
@@ -11,7 +11,7 @@ const listen = () => {
         alert(text)
         current = document.getElementsByClassName("story")[0]
         alert(current.innerHTML)
-        current.innerHTML = text
+        //current.innerHTML = text
       })
     }
   }
