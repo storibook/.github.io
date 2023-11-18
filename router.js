73d040e1f91c8e1e138de2c2d07649bbac470c23
amohebbi <amohebbi@mathworks.com>
2023-11-18 15:53:09 -0600

    enhanced routing

--- router.js
+++ router.js
@@ -5,7 +5,12 @@ const listen = () => {
   const route = () => {
     var hash = window.location.hash.slice(1)
     if (hash == "") {
-      //route to index
+        getPage("home").then((text) =>{
+            alert(text)
+            current = document.getElementsByClassName("story")[0]
+            alert(current.innerHTML)
+            current.innerHTML = text
+          })
     } else {
       //change html
       getPage(hash).then((text) =>{
@@ -18,6 +23,7 @@ const listen = () => {
   }
 
   const getPage = async (hash) => {
-    const page = await fetch("pages/second.html").then((response) => response.text())
+    location = "pages/" + hash
+    const page = await fetch(location).then((response) => response.text())
     return page
   }
