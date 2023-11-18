dd6212eb5eb887956058b5e099d1051bb12bfad7
amohebbi <amohebbi@mathworks.com>
2023-11-18 14:11:10 -0600

    Testing routing

---
+++ router.js
@@ -0,0 +1,21 @@
+const listen = () => {
+    window.addEventListener("hashchange", route)
+  }
+  
+  const route = () => {
+    var hash = window.location.hash.slice(1)
+    if (hash == "") {
+      //route to index
+    } else {
+      //change html
+      const text = getPage(hash)
+      document.getElementsByClassName("plaque").innerHTML = text;
+    }
+  }
+
+  const getPage = async (hash) => {
+    alert("in fn 1")
+    const page = await fetch("pages/second.html")
+    alert("in fn 2")
+    return page
+  }
