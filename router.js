80d1cfbaf317b78a75516a33fe5e9955f814ccf7
amohebbi <amohebbi@mathworks.com>
2023-11-18 18:26:14 -0600

    full routing

--- router.js
+++ router.js
@@ -6,6 +6,12 @@ const listen = () => {
     var hash = window.location.hash.slice(1)
     if (hash == "") {
       //route to index
+      getPage("featured").then((text) =>{
+        alert(text)
+        current = document.getElementsByClassName("story")[0]
+        alert(current.innerHTML)
+        current.innerHTML = text
+      })
     } else {
       //change html
       getPage(hash).then((text) =>{
@@ -18,7 +24,7 @@ const listen = () => {
   }
 
   const getPage = async (hash) => {
-    file = "stories/second.txt"
+    file = "pages/" + hash + ".html"
     const page = await fetch(file).then((response) => response.text())
     return page
   }
