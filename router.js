c95eb5485c7e3e180324767e9bd345ae4ac59914
amohebbi <amohebbi@mathworks.com>
2023-11-18 20:25:00 -0600

    from array to single val

--- router.js
+++ router.js
@@ -6,13 +6,14 @@ const listen = () => {
     const path = getPath()
     var story = document.getElementsByClassName("story")[0]
 
-    getPage(path).then((data) =>{
+    getPage(path).then((data) =>{/*
       [text, ok] = data
       if (ok){
         story.innerHTML = text
       } else{
         story.innerHTML = `<p class="stitle"> can't find page </p>`
-      }
+      }*/
+      story.innerHTML = data
     })
   }
 
@@ -27,5 +28,6 @@ const listen = () => {
   const getPage = async (file) => {
     const page = await fetch(file)
     text = page.text()
-    return [text, page.ok]
+    ///return [text, page.ok]
+    return text
   }
